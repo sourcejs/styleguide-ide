6639e9a96c2b3b0746bc584c4103a4b901749e9e
Robert Haritonov <r@rhr.me>
2015-06-19 22:48:04 +0200

    add vagrant conf

---
+++ vagrant/bootstrap.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+cd /home/vagrant/styleguide-ide/test-project
+forever start node_modules/sourcejs/app.js
+
+cd /home/vagrant/styleguide-ide/watcher-build
+forever start gulp watch
+
+cd /home/vagrant/styleguide-ide/watcher-lint
+forever start gulp watch
