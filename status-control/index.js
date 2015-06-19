b4f7ea9a976984b6343824aa39f07b7b8423360b
Robert Haritonov <r@rhr.me>
2015-06-19 15:01:40 +0200

    write status of build

---
+++ status-control/index.js
@@ -0,0 +1,28 @@
+var fs = require('fs-extra');
+var path = require('path');
+var deepExtend = require('deep-extend');
+
+var config = {
+    dataPath: path.join(__dirname, '../test-project/node_modules/sourcejs-contrib-status/data', 'status.json')
+};
+
+var write = module.exports.write = function(data){
+    fs.writeJsonSync(config.dataPath, data);
+};
+
+var read = module.exports.read = function(){
+    return fs.readJsonSync(config.dataPath, {throws: false})
+};
+
+module.exports.update = function(name, obj){
+    var prevData = read();
+    var writeObj = {};
+
+    if (prevData) {
+        deepExtend(writeObj, prevData)
+    }
+
+    writeObj[name] = obj;
+
+    write(writeObj);
+};
--- watcher-build/Gulpfile.js
+++ watcher-build/Gulpfile.js
@@ -4,17 +4,53 @@ var rename = require('gulp-rename');
 var path = require('path');
 var babel = require('gulp-babel');
 var plumber = require('gulp-plumber');
-var gulpif = require('gulp-if');
+var gutil = require('gulp-util');
+var statusControl = require('../status-control');
 
 var skipError = false;
 var baseDir = '../test-project/backbase';
 
+var iteration = 0;
+var iterationStatus = {};
+
 var lessFiles = path.join(baseDir + '/**/*.less');
 var jsxFiles = path.join(baseDir + '/**/*.jsx');
 
+var errorHandler = function(err){
+    // Output an error message
+    gutil.log(gutil.colors.red('Error (' + err.plugin + '): ' + err.message));
+
+    iterationStatus.status = 'error';
+    iterationStatus.msg = err.message;
+
+    // emit the end event, to properly end the task
+    this.emit('end');
+
+    if (!skipError) {
+        endHandler();
+        process.exit();
+    }
+};
+
+var endHandler = function(){
+    console.log('FINITO');
+
+    statusControl.update('build', {
+        status: iterationStatus.status,
+        msg: iterationStatus.msg
+    });
+};
+
+gulp.task('pre', function () {
+    iterationStatus = {
+        status: 'ok'
+    };
+    iteration++;
+});
+
 gulp.task('less', function () {
     return gulp.src(lessFiles)
-        .pipe(gulpif(skipError, plumber()))
+        .pipe(plumber(errorHandler))
         .pipe(less())
         .pipe(rename(function(pathConf) {
             pathConf.dirname = path.join(pathConf.dirname, '../dist/css');
@@ -24,7 +60,7 @@ gulp.task('less', function () {
 
 gulp.task('jsx', function () {
     return gulp.src(jsxFiles)
-        .pipe(gulpif(skipError, plumber()))
+        .pipe(plumber(errorHandler))
         .pipe(babel())
         .pipe(rename(function(pathConf) {
             pathConf.dirname = path.join(pathConf.dirname, '../dist/js');
@@ -35,8 +71,12 @@ gulp.task('jsx', function () {
 gulp.task('watch', function () {
     skipError = true;
 
-    gulp.watch(lessFiles, ['less']);
-    gulp.watch(jsxFiles, ['jsx']);
+    gulp.run('default');
+
+    gulp.watch([
+        lessFiles,
+        jsxFiles
+    ], ['default']);
 });
 
-gulp.task('default', ['less', 'jsx']);
+gulp.task('default', ['pre', 'less', 'jsx'], endHandler);
